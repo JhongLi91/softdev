e1f1e58a77d872e20886c4f51ae1c32d41640b15
Jian Hong Li <78228997+JhongLi91@users.noreply.github.com>
2022-09-21 21:48:43 -0400

    Update warmup1.py

--- 03_py/warmup1.py
+++ 03_py/warmup1.py
@@ -32,3 +32,153 @@ def front3(str):
     return str * 3 #if so, just copies the entire str 3 times
   front = str[0:3] #takes the first 3 chars
   return front * 3 #copies front 3 times
+  
+ '''
+Return a new string where the first and last chars have been exchanged.
+front_back('code') → 'eodc'
+front_back('a') → 'a'
+front_back('ab') → 'ba'
+front_back('abc') → 'cba'
+front_back('') → ''
+'''
+
+def front_back(str):
+  if (len(str) <= 1): #if length is less or equal to 1, there is no difference between the first and last chars
+    return str        #thus, you would just return the str
+  return str[len(str)-1] + str[1:len(str)-1] + str[0:1]
+ 
+'''
+Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
+makes10(9, 10) → True	
+makes10(9, 9) → False	
+makes10(1, 9) → True	
+makes10(10, 1) → True	
+DISCO: Similar to Java, you do not need an else statement to just go directly into the 
+statement if the "if" statement isn't triggered. 
+'''
+
+def makes10(a, b):
+  if a == 10 or b == 10 or b + a == 10: #returned True if one or their sum is 10. 
+    return True
+  return False
+'''
+Return a new string where the char at index n has been removed. 
+missing_char('kitten', 1) → 'ktten'	
+missing_char('kitten', 0) → 'itten'
+missing_char('kitten', 4) → 'kittn'
+missing_char('Hi', 0) → 'i'
+DISCO: Very cool discovery here. To get the substring of a string from index n to the end, 
+all you need to do is [n:]. Python automatically assumes that it goes to the last index. 
+'''
+def missing_char(str, n):
+  return str[0:n] + str[n+1:] #took substring before index n and the substring after it & combined them together
+'''
+Return true if both or neither are smiling. 
+monkey_trouble(True, True) → True	True	
+monkey_trouble(False, False) → True	True	
+monkey_trouble(True, False) → False	False	
+monkey_trouble(False, True) → False	False	
+'''
+def monkey_trouble(a_smile, b_smile):
+  if (a_smile) == (b_smile): #if both are the same (smiling or not smiling) return True
+    return True
+    
+  else:
+    return False
+'''
+Given an int n, return True if it is within 10 of 100 or 200.
+near_hundred(93) → True
+near_hundred(90) → True	
+near_hundred(89) → False
+near_hundred(110) → True	
+'''
+
+def near_hundred(n):
+  if abs(100-n) <= 10 or abs(200-n) <= 10: #used abs to find the difference b/c it doesn't matter whether it is positive or negative difference 
+    return True
+  return False
+  
+'''
+Given a string, return a new string where "not " has been added to the front. 
+However, if the string already begins with "not", return the string unchanged.
+not_string('candy') → 'not candy'
+not_string('x') → 'not x'
+not_string('not bad') → 'not bad'
+not_string('bad') → 'not bad'
+DISCO: First DISCO of the warmup! You can combine strings just by using the + sign. 
+'''
+
+def not_string(str):
+  if str[:3] == "not": #if string began with not, then it would be unchanged. 
+    return str
+  return "not" + str 
+  
+'''
+Return true if talking = true and hour is before 7 or after 20. 
+parrot_trouble(True, 6) → True
+parrot_trouble(True, 7) → False
+parrot_trouble(False, 6) → False	
+parrot_trouble(True, 21) → True
+parrot_trouble(False, 21) → False	
+'''
+
+def parrot_trouble(talking, hour):
+  if talking == True and (hour < 7 or hour > 20):
+    return True
+    
+  else:
+    return False
+'''
+Given 2 int values, return True if one is negative and one is positive. 
+Except if the parameter "negative" is True, then return True only if both are negative.
+pos_neg(1, -1, False) → True
+pos_neg(-1, 1, False) → True
+pos_neg(-4, -5, True) → True
+pos_neg(-4, -5, False) → False
+'''
+
+def pos_neg(a, b, negative):
+  if negative == True:  #triggers second req since parameter "negative" is True
+    if a < 0 and b < 0: #checked for the second req where you return True only if both are negative
+      return True
+    else:
+      return False
+      
+  if (a > 0 and b > 0) or (a < 0 and b < 0): #checked if both are pos or both are neg
+    return False
+  
+  return True
+  
+ '''
+The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. 
+We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
+>>> sleep_in(True, False)
+False
+>>> sleep_in(False, False)
+True
+>>> sleep_in(True, True)
+True
+>>> sleep_in(False, True)
+True
+'''
+
+def sleep_in(weekday, vacation):
+    if not weekday or vacation:
+      return True
+    else:
+      return False
+      
+'''
+Return the sum of the given two ints...
+Unless the two values are the same, then return double their sum.
+sum_double(1, 2) → 3
+sum_double(3, 2) → 5
+sum_double(2, 2) → 8
+sum_double(-1, 0) → -1
+'''
+
+def sum_double(a, b):
+  if (a == b):  #checked if the two values were the same
+    return (a+b)*2  #returned double
+    
+  return a + b
