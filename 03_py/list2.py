7c42773465676108de680a9c3d0adc193ad50140
Jian Hong Li <78228997+JhongLi91@users.noreply.github.com>
2022-09-21 21:33:02 -0400

    Update list2.py

--- 03_py/list2.py
+++ 03_py/list2.py
@@ -18,6 +18,10 @@ def big_diff(nums):
     ma = max(e, ma)
     
   return (ma - mi)
+  
+# big_diff([2, 3]) → 1	
+# big_diff([2, 2]) → 0	
+# big_diff([2]) → 0	
 
 def centered_average(nums):
   mi = nums[0]
@@ -28,6 +32,10 @@ def centered_average(nums):
     ma = max(e, ma)
     s += e
   return (s - mi - ma) / (len(nums) - 2)
+
+# centered_average([4, 4, 4, 4, 5]) → 4	
+# centered_average([4, 4, 4, 1, 5]) → 4	
+# centered_average([6, 4, 8, 12, 3]) → 6		
 
 def sum13(nums):
   if len(nums) == 0:
@@ -42,6 +50,10 @@ def sum13(nums):
       s += nums[i]
       i+=1
   return s
+
+# sum13([5, 13, 2]) → 5	
+# sum13([0]) → 0	
+# sum13([13, 0]) → 0
 
 def sum67(nums):
   if len(nums) == 0:
@@ -60,6 +72,10 @@ def sum67(nums):
       i+=1
       
   return s
+  
+# sum67([6, 7, 11]) → 11	11	OK	
+# sum67([11, 6, 7, 11]) → 22	22	OK	
+# sum67([2, 2, 6, 7, 7]) → 11	11	OK
 
 def has22(nums):
   i = 0 
@@ -70,3 +86,7 @@ def has22(nums):
     i+=1
     
   return False
+
+# has22([]) → False	
+# has22([3, 3, 2, 2]) → True		
+# has22([5, 2, 5, 2]) → False	
