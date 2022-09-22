ff2884e5b30ec58706b9fb2db213ee21588fa993
Jian Hong Li <78228997+JhongLi91@users.noreply.github.com>
2022-09-21 22:19:40 -0400

    Update solutions.py

--- 03_py/warmup1.py/solutions.py
+++ 03_py/warmup1.py/solutions.py
@@ -78,9 +78,16 @@ all you need to do is [n:]. Python automatically assumes that it goes to the las
 '''
 def missing_char(str, n):
   return str[0:n] + str[n+1:] #took substring before index n and the substring after it & combined them together
+
+print(missing_char('kitten', 1)) # → 'ktten'	
+print(missing_char('kitten', 0)) # → 'itten'
+print(missing_char('kitten', 4)) #→ 'kittn'
+print(missing_char('Hi', 0)) # → 'i'
+
 '''
 Return true if both or neither are smiling. 
 '''
+
 def monkey_trouble(a_smile, b_smile):
   if (a_smile) == (b_smile): #if both are the same (smiling or not smiling) return True
     return True
