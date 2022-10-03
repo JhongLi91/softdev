765968d3534d9bc4690067efd5546c39d9232df6
Jian Hong Li <78228997+JhongLi91@users.noreply.github.com>
2022-10-03 14:02:22 -0400

    Create flask.py

---
+++ 07_teardown/flask.py
@@ -0,0 +1,33 @@
+# your heading here
+
+from flask import Flask
+
+app = Flask(__name__) # Q0: Where have you seen similar syntax in other langs?
+
+@app.route("/") # Q1: What points of reference do you have for meaning of '/'?
+def hello_world():
+    print(__name__) # Q2: Where will this print to? Q3: What will it print?
+    return "No hablo queso!"  # Q4: Will this appear anywhere? How u know?
+
+app.run()  # Q5: Where have you seen similar constructs in other languages?
+
+
+'''
+DISCO:
+QCC:
+1. What is app
+
+...
+0. Stack in java?
+1. In python, '/' continues the next line to the current line
+2. app
+3. '/'
+4. 
+5. 
+
+...
+INVESTIGATIVE APPROACH:
+<Your concise summary of how
+ you and your team set about
+ "illuminating the cave of ignorance" here...>
+'''
