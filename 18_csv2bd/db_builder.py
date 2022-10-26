64a76053315a08392028b90e5db0471265e06213
JhongLi91 <lijh0901@gmail.com>
2022-10-26 13:28:26 -0400

    18

--- 18_csv2bd/db_builder.py
+++ 18_csv2bd/db_builder.py
@@ -7,7 +7,9 @@ import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
 
 
-DB_FILE="discobandit.db"
+
+
+DB_FILE="courses"
 
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
@@ -18,12 +20,18 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 # < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
 
 
-command = ""          # test SQL stmt in sqlite3 shell, save as string
+command = "CREATE TABLE rosters(name TEXT, mark INTEGER);"
+# test SQL stmt in sqlite3 shell, save as string
 c.execute(command)    # run SQL statement
 
+with open('courses.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        row_data = row['code']
+        mark_data = row['mark']
+        c.execute("INSERT INTO rosters VALUES(" + '"' +  row_data + '"'  + "," + mark_data + ");")
 #==========================================================
 
 db.commit() #save changes
 db.close()  #close database
-
 
