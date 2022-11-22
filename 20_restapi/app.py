83501cd2ba9f3885cb26861f64bf20d3bb0dbe60
JhongLi91 <lijh0901@gmail.com>
2022-11-22 13:39:19 -0500

    20

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -1,15 +1,28 @@
-from flask import Flask
-from flask import render_template
-import urllib.request, json
+"""
+Kung Pao Chicken: Brian Yang, Jian Hong li
+SoftDev
+k20 -- RESTful API
+2022 -- 11 -- 21
+time spent: .5 hours
+"""
 
-#______________________
+from flask import Flask, render_template
+import requests
+
 app = Flask(__name__)    #create Flask object
+
 
 @app.route("/")
 def index():
-    key = open("key_nasa.txt").read()
-    url = "https://api.nasa.gov/planetary/apod?api_key=" + key
-    return render_template("main.html", url = url)
+    try: #check for if key_nasa.txt exists
+        with open("key_nasa.txt", "r") as file:
+            api_key = file.read()
+            request_data = requests.get("https://api.nasa.gov/planetary/apod?api_key=" + api_key) #returns a requests.models.Response class that need to be made into a json
+            data = request_data.json()
+            image = data["hdurl"] #Select the thing we want from the json
+            return render_template('main.html', img = image)
+    except:
+        return render_template('main.html', error = "No/Wrong API key provided")
 
 
 if __name__ == "__main__": #false if this file imported as module
